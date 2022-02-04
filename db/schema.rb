4a45557d05cdcc816296815c5912c323fb34d001
Vi Truong <vitruong00@tamu.edu>
2022-02-03 21:45:07 -0600

    Deployment testing

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2022_02_02_200902) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
-    t.float "price"
+    t.decimal "price"
     t.date "publishedDate"
   end
 
